e9d6aa9b587ce278039a809c99a46a43c66d6a14
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-14 20:14:55 +0300

    Create helpers customError, ctrlWrapper, handleMongooseError

---
+++ utils/index.js
@@ -0,0 +1,5 @@
+const customError = require('./customError');
+const ctrlWrapper = require('./ctrlWrapper');
+const handleMongooseError = require('./handleMongooseError');
+
+module.exports = { customError, ctrlWrapper, handleMongooseError };
