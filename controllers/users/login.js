f75ffb574b16a07a4edf46ec6ae1bc8c00028120
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-14 20:17:08 +0300

    Create user controllers register, login, logout, current

---
+++ controllers/users/login.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../../models');
+const { ctrlWrapper, customError } = require('../../utils');
+
+const { JWT_SECRET_KEY } = process.env;
+
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) throw customError(401, 'Email or password is not valid');
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw customError(401, 'Email or password is not valid');
+  }
+  const jwtPayload = {
+    id: user._id,
+  };
+  const token = jwt.sign(jwtPayload, JWT_SECRET_KEY, { expiresIn: '24h' });
+  await User.findByIdAndUpdate(user._id, { token });
+
+  res.status(200).json({ token, email });
+};
+
+module.exports = { login: ctrlWrapper(login) };
