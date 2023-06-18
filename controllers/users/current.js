7742a61c96f02034428721ce0fe7550ef5a25dd9
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-18 20:39:58 +0300

    Update current.js

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -2,7 +2,7 @@ const { ctrlWrapper } = require('../../utils');
 
 const current = async (req, res, next) => {
   const { username, email, token } = req.user;
-  res.status(200).json({ username, email, token });
+  res.status(200).json({ username, email, token, message: 'Login successful' });
 };
 
 module.exports = { current: ctrlWrapper(current) };
