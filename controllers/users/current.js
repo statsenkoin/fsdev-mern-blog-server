66dc43269b392f5bbecad0a64ba29b640111e6ad
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-18 19:52:10 +0300

    Update response

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -1,8 +1,8 @@
 const { ctrlWrapper } = require('../../utils');
 
 const current = async (req, res, next) => {
-  const { _id, username, email } = req.user;
-  res.status(200).json({ _id, username, email });
+  const { username, email, token } = req.user;
+  res.status(200).json({ username, email, token });
 };
 
 module.exports = { current: ctrlWrapper(current) };
