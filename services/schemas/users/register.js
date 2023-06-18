11ea092054c0662c003d772888a3bfd15fb4844a
Ihor Statsenko <statsenkoin@gmail.com>
2023-06-18 19:53:31 +0300

    Add allow("") to username validation

--- services/schemas/users/register.js
+++ services/schemas/users/register.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const { emailRegExp } = require('../../../config/constants');
 
 const register = Joi.object({
-  username: Joi.string(),
+  username: Joi.string().allow(''),
   email: Joi.string().required().pattern(emailRegExp),
   password: Joi.string().required().min(6),
 });
